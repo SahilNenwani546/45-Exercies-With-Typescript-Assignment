4105938beae39b8790671963e9f05f9afd1302af
Sahil Nenwani <164480490+SahilNenwani546@users.noreply.github.com>
2024-04-18 17:57:56 +0500

    Update app.js

--- Task_22(Intentional_Error)/app.js
+++ Task_22(Intentional_Error)/app.js
@@ -3,7 +3,7 @@ try to make one happen. Change an index in one of your programs to produce an in
 correct the error before closing the program.*/
 // Creating a error
 var errorArray = ["A", "B", "C", "D"];
-// Producing Error ! by Accessing invalid index of arrray
-// console.log(errorArray[6]);
+Producing Error ! by Accessing invalid index of arrray
+console.log(errorArray[6]);
 // Error Removed
 console.log(errorArray[1]);
